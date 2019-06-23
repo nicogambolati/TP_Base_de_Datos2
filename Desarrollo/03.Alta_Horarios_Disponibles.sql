53dc8e982e62d9946b7155499a7f7873288a6abf
nicogambolati <48931457+nicogambolati@users.noreply.github.com>
2019-06-23 19:59:47 -0300

    Delete 03.Alta_Horarios_Disponibles.sql

--- Desarrollo/03.Alta_Horarios_Disponibles.sql
+++
@@ -1,9 +0,0 @@
-﻿CREATE PROCEDURE Alta_Horarios_Disponibles
-	@id_servicio_profesional int,
-	@es_profesional bit,
-	@fecha_hora_inicio datetime,
-	@fecha_hora_fin datetime
-AS
-BEGIN
-	INSERT INTO Horarios_disponibles (id_servicio_profesional, es_profesional, fecha_hora_inicio, fecha_hora_fin) VALUES (@id_servicio_profesional, @es_profesional, @fecha_hora_inicio, @fecha_hora_fin)
-END
