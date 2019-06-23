7b3f98ac89fa6db4864643e1f3de8472f8c1de14
nicogambolati <48931457+nicogambolati@users.noreply.github.com>
2019-06-23 20:01:18 -0300

    Delete 16.Validar_Login_Paciente.sql

--- Desarrollo/16.Validar_Login_Paciente.sql
+++
@@ -1,8 +0,0 @@
-CREATE PROCEDURE validar_login_paciente
-	@paciente varchar(255)
-AS BEGIN
-	IF EXISTS (SELECT * FROM Paciente p WHERE p.nombre = @paciente)
-		PRINT 'Paciente encontrado'
-	ELSE
-		PRINT 'Paciente NO encontrado'
-END
