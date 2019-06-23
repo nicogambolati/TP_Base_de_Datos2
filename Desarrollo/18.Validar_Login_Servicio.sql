8a321ce65495f8a5e2bcd2d73f5ff22c5c35f391
nicogambolati <48931457+nicogambolati@users.noreply.github.com>
2019-06-23 20:01:33 -0300

    Delete 18.Validar_Login_Servicio.sql

--- Desarrollo/18.Validar_Login_Servicio.sql
+++
@@ -1,8 +0,0 @@
-CREATE PROCEDURE validar_login_servicio
-	@servicio varchar(50)
-AS BEGIN
-	IF EXISTS (SELECT * FROM Profesional p WHERE p.nombre = @servicio)
-		PRINT 'Servicio encontrado'
-	ELSE
-		PRINT 'Servicio NO encontrado'
-END
