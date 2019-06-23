09105153fc202a4ae2fd8626ca38114c2d170b83
nicogambolati <48931457+nicogambolati@users.noreply.github.com>
2019-06-23 19:59:38 -0300

    Delete 02.Alta_Profesional.sql

--- Desarrollo/02.Alta_Profesional.sql
+++
@@ -1,9 +0,0 @@
-﻿CREATE PROCEDURE  Alta_Profesional
-	@nombre varchar(255),
-	@email varchar (80),
-	@telefono varchar (40)
-AS
-BEGIN
-	INSERT INTO Profesional (nombre,email,telefono,baja) VALUES (@nombre,@email,@telefono,0)
-	PRINT '* * * SE HA DADO DE ALTA AL PROFESIONAL * * *'
-END
