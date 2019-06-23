8c7ff6f8a2175286aa2b41684b300eb151e0b611
nicogambolati <48931457+nicogambolati@users.noreply.github.com>
2019-06-23 20:01:25 -0300

    Delete 17.Validar_Login_Profesional.sql

--- Desarrollo/17.Validar_Login_Profesional.sql
+++
@@ -1,8 +0,0 @@
-CREATE PROCEDURE validar_login_profesional
-	@profesional varchar(50)
-AS BEGIN
-	IF EXISTS (SELECT * FROM Profesional p WHERE p.nombre = @profesional)
-		PRINT 'Profesional encontrado'
-	ELSE
-		PRINT 'Profesional NO encontrado'
-END
