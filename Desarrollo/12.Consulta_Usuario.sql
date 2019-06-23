ff0ea6d544b62ce5c9ffcb8bc58533296cce2d42
nicogambolati <48931457+nicogambolati@users.noreply.github.com>
2019-06-23 20:00:51 -0300

    Delete 12.Consulta_Usuario.sql

--- Desarrollo/12.Consulta_Usuario.sql
+++
@@ -1,8 +0,0 @@
-CREATE PROCEDURE consulta_usuario
-	@usuario varchar(50), @filas_por_pag int, @num_pag int
-
-	AS BEGIN
-		SELECT id, usuario, tipo_actividad
-		FROM Auditoria
-		WHERE usuario = @usuario AND id <= @filas_por_pag*@num_pag AND id > (@num_pag-1)*@filas_por_pag
-	END
